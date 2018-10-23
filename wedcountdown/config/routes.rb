644a6b370f62399c158129934a433aa19419a72b
bodebogdan <ella92mp@yahoo.com>
2018-10-23 19:22:57 +0300

    Updating style

--- wedcountdown/config/routes.rb
+++ wedcountdown/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'weddings/show'
   resources :weddings
+  root 'weddings#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
